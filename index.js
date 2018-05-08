434dc57bead0e59e1a266419e9eb5905cc59ee86
Nicholas Martin <nicholas.martin89@gmail.com>
2018-05-07 21:35:31 -0700

    corrected all functions except '+/-' function

--- index.js
+++ index.js
@@ -4,17 +4,21 @@
 const calculatorDisplay = document.getElementById('calculator-display');
 let memory = 0;
 //create an array to hold current number as well as commands in order
-let currentFunction = [0];
+let currentFunction = [];
 let lastElement = currentFunction[currentFunction.length - 1];
-let currentNumber = Number(currentFunction[lastElement].toString());
+//let currentNumber = Number(currentFunction[lastElement].toString());
 
 let currentDisplay = calculatorDisplay.innerHTML;
+let lastHit;
+let toSolve;
+let solution;
 
 /**
  * Set the value of what is displayed on the calculator.
  */
 function setCalculatorDisplay (val) {
     calculatorDisplay.innerHTML = val;
+    currentDisplay = val;
 }
 
 function updateCalculatorDisplay (val) {
@@ -56,12 +60,14 @@ function test () {
 }
 
 function clearDisplay () {
-    currentFunction = [0];
-    setCalculatorDisplay (currentFunction[0]);
+    currentFunction = [];
+    setCalculatorDisplay (0);
+    lastHit = undefined;
 }
 
 function pressNumber (num) {
     updateCalculatorDisplay (num);
+    lastHit = num;
 }
 
 
@@ -70,11 +76,62 @@ function pressNumber (num) {
  * to properly show current number
  */
 function callOperator(button) {
-    if (lastElement === 0) {
-        currentFunction.splice(currentFunction.length - 1, 1, currentDisplay);
+
+    if (button === '=') {    // if '=' is hit
+        if (lastHit === '=') {   // is '=' is hit a second time in a row, repeat last function
+            if (toSolve.length > 1) {
+                let solveAgain = [currentDisplay, toSolve[1], toSolve[2]];
+                console.log(solveAgain);
+                solution = eval(solveAgain.join(''));
+                console.log(solution);
+                currentFunction = [solution];
+                setCalculatorDisplay(solution);
+            } else {
+                setCalculatorDisplay(currentFunction[0]);
+            }
+        } else if (!isNaN(lastHit)) {    // if lastHit is a number
+            currentFunction.push(currentDisplay);
+            toSolve = currentFunction.splice(0, 3);
+            solution = eval(toSolve.join(''));
+            currentFunction = [solution];
+            console.log(currentFunction);
+            setCalculatorDisplay(solution);
+        }
+
+    } else if (button !== '=') {    //if an operator other than '=' is hit
+        if (lastHit == undefined) {  //first hit is an operator
+            currentFunction.push(currentDisplay);
+            currentFunction.push(button);
+            setCalculatorDisplay(0);
+        } else if (isNaN(lastHit) && isNaN(lastElement)) {     //if lastHit and lastElement are operators 
+            currentFunction.splice(currentFunction.length - 1, 1, button);
+        } else if (isNaN(lastHit) && !isNaN(lastElement)) {    // if lastHit was an operator and lastElement is a number
+            currentFunction.push(button);
+            setCalculatorDisplay(0);
+        }
+        else if (!isNaN(lastHit)) {   // if lastHit is a number
+            currentFunction.push(currentDisplay);
+            currentFunction.push(button);
+            setCalculatorDisplay(0);
+        } else if (!isNaN(lastElement)) {  // if lastElement is a number
+            currentFunction.push(button);
+            setCalculatorDisplay(0);
+        }
     }
-        currentFunction.push(button);
-        currentFunction.push(0);
-        setCalculatorDisplay (0);
     
+    
+    if (currentFunction.length >= 3) {
+        toSolve = currentFunction.splice(0, 3);
+        solution = eval(toSolve.join(''));
+        if (lastHit === '=') {
+            currentFunction = [solution];
+            setCalculatorDisplay(solution);
+        } else {
+            currentFunction.splice(0, 0, solution);
+        }
+    }
+    lastHit = button;
+    lastElement = currentFunction[currentFunction.length - 1];
+
 }
+
